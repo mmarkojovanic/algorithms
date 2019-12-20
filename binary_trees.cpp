d50b63f518c04c98ab8e4b5dfdf80c449551272d
mmarkojovanic <mmarkojovanic@gmail.com>
2019-12-20 12:44:21 +0100

    Update binary_trees.cpp

--- binary_trees.cpp
+++ binary_trees.cpp
@@ -86,30 +86,19 @@ public:
 	void in_order()
 	{
 		cout << "inorder trav" << endl;
-		// create an empty stack
 		stack<Node*> stack;
-
-		// start from root node (set current node to root node)
 		Node *curr = root;
-
-		// if current node is null and stack is also empty, we're done
 		while (!stack.empty() || curr != nullptr)
 		{
-			// if current node is not null, push it to the stack (defer it)
-			// and move to its left child
-			if (curr != nullptr)
+			while (curr != nullptr)
 			{
 				stack.push(curr);
 				curr = curr->left;
 			}
-			else
-			{
-				// else if current node is null, we pop an element from stack,
-				// print it and finally set current node to its right child
-				curr = stack.top();
-				stack.pop();
-				cout << curr->num << " ";
-				curr = curr->right;
+			curr = stack.top();
+			stack.pop();
+			cout << curr->num << " ";
+			curr = curr->right;
 			}
 		}
 		cout << "\n";
